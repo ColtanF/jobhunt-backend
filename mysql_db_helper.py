3bf30b02b60147768de080cf87e542372faaebdd
ColtanF <coltan.franke@gmail.com>
2021-08-30 16:55:18 -0500

    deploy debugging...

--- mysql_db_helper.py
+++ mysql_db_helper.py
@@ -61,6 +61,7 @@ def checkAndMakeDB():
     )
     db_cursor = db_connection.cursor()
 
+    print("db value = " + db)
     db_cursor.execute("SHOW DATABASES;")
     databases = [item[0] for item in db_cursor.fetchall()]
 
